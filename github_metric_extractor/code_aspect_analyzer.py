29930d0bbaf72d80b16bcbe936a573cd4bc19b40
Tobias Hansson <tobiashansson93@gmail.com>
2024-02-26 14:15:31 +0100

    Capture and structure content

--- github_metric_extractor/code_aspect_analyzer.py
+++ github_metric_extractor/code_aspect_analyzer.py
@@ -1,39 +1,68 @@
-from pylint.checkers import BaseChecker
 from pylint.lint import Run
 from pylint.reporters.text import TextReporter
+from pylint.message import Message
 from io import StringIO
 
 
-class SomeReporter(TextReporter):
+class LintReporter(TextReporter):
     def __init__(self, output=None):
-        super().__init__(output)
-        self.content = []
+        super().__init__(output=output)
+        self.messages = []
 
-    def write(self, string):
-        self.content.append(string)
+    def _display(self, layout):
+        pass
 
-    def read(self):
-        return self.content
+    def handle_message(self, msg: Message):
+        self.messages.append(msg)
 
 
 def classify_lint_messages(messages):
-    classification = {'error': [], 'warning': [], 'refactor': [], 'convention': [], 'fatal': []}
+    result = {}
     for msg in messages:
-        if msg.category in classification:
-            classification[msg.category].append(msg)
-    return classification
+        module = msg.module
+        if module not in result:
+            result[module] = {'total_messages': 0, 'categories': {}}
+
+        category = msg.category
+        if category not in result[module]['categories']:
+            result[module]['categories'][category] = {'total': 0, 'message_ids': {}}
+
+        msg_id = msg.msg_id
+        if msg_id not in result[module]['categories'][category]['message_ids']:
+            result[module]['categories'][category]['message_ids'][msg_id] = {
+                'count': 0,
+                'message': msg.msg,
+                'occurrences': []
+            }
+
+        result[module]['total_messages'] += 1
+        result[module]['categories'][category]['total'] += 1
+        result[module]['categories'][category]['message_ids'][msg_id]['count'] += 1
+        result[module]['categories'][category]['message_ids'][msg_id]['occurrences'].append({
+            'line': msg.line,
+            'column': msg.column,
+        })
+
+    return result
 
 
 if __name__ == '__main__':
     target_files = ["../pylint-test.py", "../pylint-test2.py"]
     out = StringIO()
-    reporter = SomeReporter(out)
+    reporter = LintReporter(output=out)
     Run(target_files, reporter=reporter, exit=False)
+    print("-" * 20)
+    print(reporter.messages)
+    print("-" * 20)
     classification = classify_lint_messages(reporter.messages)
-    print(classification)
-    print("NIFTY LITTLE LINE TO SHARE WITH YOUR BUDDIES")
-    for line in out.read():
-        print(line)
-    print("NIFTY LITTLE LINE TO SHARE WITH YOUR BUDDIES")
-    for k, v in classification.items():
-        print(f"{k}: {[vi + ' Ass ' for vi in v]}")
+
+    print("\nClassification Results:")
+    for module, info in classification.items():
+        print(f"Module: {module}, Total Messages: {info['total_messages']}")
+        for category, cat_info in info['categories'].items():
+            print(" " * 2 + f"Category: {category}, Total: {cat_info['total']}")
+            for msg_id, msg_info in cat_info['message_ids'].items():
+                print(" " * 4 + f"Message ID: {msg_id}, Count: {msg_info['count']}")
+                print(" " * 6 + f"Message: {msg_info['message']}")
+                for occurrence in msg_info['occurrences']:
+                    print(" " * 8 + f"Occurrence: Line {occurrence['line']}, Column {occurrence['column']}")
