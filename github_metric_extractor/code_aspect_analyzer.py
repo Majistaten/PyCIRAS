b9a9471561bb9eab985de16f351f0752c87e2f69
Tobias Hansson <tobiashansson93@gmail.com>
2024-02-26 17:23:47 +0100

    Repo analyze

--- github_metric_extractor/code_aspect_analyzer.py
+++ github_metric_extractor/code_aspect_analyzer.py
@@ -2,6 +2,8 @@ from pylint.lint import Run
 from pylint.reporters.text import TextReporter
 from pylint.message import Message
 from io import StringIO
+import os
+import pprint
 
 
 class LintReporter(TextReporter):
@@ -16,7 +18,7 @@ class LintReporter(TextReporter):
         self.messages.append(msg)
 
 
-def classify_lint_messages(messages):
+def lint_message_extraction(messages):
     result = {}
     for msg in messages:
         module = msg.module
@@ -31,6 +33,7 @@ def classify_lint_messages(messages):
         if msg_id not in result[module]['categories'][category]['message_ids']:
             result[module]['categories'][category]['message_ids'][msg_id] = {
                 'count': 0,
+                'symbol': msg.symbol,
                 'message': msg.msg,
                 'occurrences': []
             }
@@ -46,23 +49,45 @@ def classify_lint_messages(messages):
     return result
 
 
-if __name__ == '__main__':
-    target_files = ["../pylint-test.py", "../pylint-test2.py"]
+def get_python_files_from_directory(directory):
+    python_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".py"):
+                print(f"Found: {str(os.path.join(root, file))}")
+                python_files.append(str(os.path.join(root, file)))
+    return python_files
+
+
+def analyze_repository(repository_path):
+    result = {}
+    target_files = get_python_files_from_directory(repository_path)
     out = StringIO()
     reporter = LintReporter(output=out)
-    Run(target_files, reporter=reporter, exit=False)
-    print("-" * 20)
-    print(reporter.messages)
-    print("-" * 20)
-    classification = classify_lint_messages(reporter.messages)
+    run = Run(target_files, reporter=reporter, exit=False)
+    stats = run.linter.stats
+    if not isinstance(stats, dict):
+        stats_dict = {attr: getattr(stats, attr) for attr in dir(stats) if
+                      not attr.startswith('__') and not callable(getattr(stats, attr))}
+    else:
+        stats_dict = stats
+
+    result['messages'] = lint_message_extraction(reporter.messages)
+    result['stats'] = stats_dict
+
+    return result
+
+
+if __name__ == '__main__':
+    classification = analyze_repository("../repositories/NDStudy")
 
     print("\nClassification Results:")
-    for module, info in classification.items():
+    for module, info in classification["messages"].items():
         print(f"Module: {module}, Total Messages: {info['total_messages']}")
         for category, cat_info in info['categories'].items():
             print(" " * 2 + f"Category: {category}, Total: {cat_info['total']}")
             for msg_id, msg_info in cat_info['message_ids'].items():
                 print(" " * 4 + f"Message ID: {msg_id}, Count: {msg_info['count']}")
-                print(" " * 6 + f"Message: {msg_info['message']}")
-                for occurrence in msg_info['occurrences']:
-                    print(" " * 8 + f"Occurrence: Line {occurrence['line']}, Column {occurrence['column']}")
+                print(" " * 6 + f"Symbol: {msg_info['symbol']}")
+
+    pprint.pprint(classification["stats"])
