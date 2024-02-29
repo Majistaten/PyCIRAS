b99b69be59e28474aa9409f153fcf921c3650e63
Samuel Thand <sath2102@student.miun.se>
2024-02-29 11:23:16 +0100

    deleted test code

--- github_metric_extractor/csv_builder.py
+++ github_metric_extractor/csv_builder.py
@@ -22,31 +22,11 @@ def json_to_csv(json_data, output_directory, csv_file_name):
     # Full path for the CSV file
     csv_file_path = output_directory / csv_file_name
 
-    # Determine the CSV column headers from the keys of the first JSON object
-    # headers = json_data[0].keys()
     headers = json_data.keys()
 
-    # TODO only works for flat JSON, need to flatten the data.
-    # TODO Check Pandas or something
-    # Writing to CSV
-    with csv_file_path.open(mode='w', newline='', encoding='utf-8') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=headers)
-        writer.writeheader()
-        # for data in json_data:
-        for item in json_data:
-            # Ensure item is a non-empty string that should contain JSON
-            if isinstance(item, str) and item.strip():
-                try:
-                    data = json.loads(item)
-                    writer.writerow(data)
-                except json.JSONDecodeError as e:
-                    print(f"Error decoding JSON: {e.msg} at line {e.lineno} column {e.colno}")
-            else:
-                # If item is already a dictionary or not a valid JSON string, handle accordingly
-                if isinstance(item, dict):
-                    writer.writerow(item)
-                else:
-                    print("Item is not a valid JSON string:", item)
+    # TODO logic for flattening the CSV and writing to files
+
+
 
 
 def read_json_from_file(file_path):
@@ -61,8 +41,8 @@ def read_json_from_file(file_path):
 
 
 if __name__ == "__main__":
-    json_file_path = Path('test_out.json')  # Update this path
-    csv_file_name = 'test_csv.csv'  # Define your CSV file name
+    json_file_path = Path('test_out.json')
+    csv_file_name = 'test_csv.csv'
 
     # Define the output directory one level up and inside a new "csv" folder
     output_directory = Path(__file__).parent.parent / 'csv'
