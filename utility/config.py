1c101332d2c7c1d14f2b779a9dc1d68a96b22a86
Samuel Thand <sath2102@student.miun.se>
2024-03-27 18:38:38 +0100

    Added a bunch of directories and patterns to ignore

--- utility/config.py
+++ utility/config.py
@@ -11,10 +11,56 @@ LOGGING_FOLDER: Path = OUTPUT_FOLDER / 'logs'
 GRAPHQL_API: str = 'https://api.github.com/graphql'
 PYLINT_CONFIG: Path = ROOT_DIR / 'mining' / '.pylintrc'
 
+IGNORE_STARTSWITH: tuple = ('.', '~', '_', '#', '$', '!', '%', '+')
+IGNORE_DIRECTORIES: list[str] = [
+    # Common virtual environments
+    'env', 'venv', 'env.bak', 'venv.bak',
+
+    # Version control
+    'vcs',
+
+    # Dependency directories
+    'node_modules', 'bower_components',
+
+    # Build/output directories
+    'build', 'dist', 'lib', 'lib64', 'bin', 'wheelhouse', 'wheels',
+    'develop-eggs', 'eggs', 'parts', 'sdist', 'var', 'share',
+
+    # Temporary/Backup files
+    'temp', 'tmp',
+
+    # Logs and data
+    'logs', 'data', 'dataset', 'datasets',
+
+    # Documentation
+    'docs', 'notes', 'wiki', 'documentation',
+
+    # Cache
+    'cache', 'target',
+
+    # Specific tool/framework directories
+    'migrations', 'assets', 'static', 'media', 'vendor', 'third_party',
+    'instance',  # Flask-specific directory
+
+    # Configuration and settings
+    'config', 'settings', 'ini', 'manifest',
+
+    # Development environment
+    'vagrant', 'docker', 'dockerfiles', 'vagrantfiles',
+
+    # IPython
+    'profile_default',
+
+    # Miscellaneous
+    'downloads',  # Often used for downloaded dependencies or data
+    'cython_debug',  # Cython debug symbols
+    'site'  # Python site-packages directory, sometimes used in a local context
+]
+
 DISABLE_PROGRESS_BARS: bool = False
 CONSOLE_LOGGING_LEVEL: int = logging.INFO
 FILE_LOGGING_LEVEL: int = logging.DEBUG
-WRITE_DATABASE: bool = True
-WRITE_JSON: bool = False
+WRITE_DATABASE: bool = False
+WRITE_JSON: bool = True
 WRITE_CSV: bool = True
 ENABLE_NTFYER: bool = False
